327663f3410b407d1ca5f878bd4a6d72ee648046
MOHAMMED MAZHAR AZIZ <40437843+remotedeveloper007@users.noreply.github.com>
2024-07-29 01:39:09 +0530

    remove unwanter import

--- resources/js/Pages/Frontend/Home/CategoryArea.jsx
+++ resources/js/Pages/Frontend/Home/CategoryArea.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { Link } from '@inertiajs/react';
 import axios from 'axios';
-import Index from '../../../Layouts/Index';
 
 const CategoryArea = () => {
   const [categories, setCategories] = useState([]);
